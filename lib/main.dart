474da17b7fa8d7c89238ab9d3db65d51424490b6
iiAtd <a.alnuss@gmail.com>
2024-04-30 19:51:24 +0300

    ....

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/screens/welcome_screen.dart';
+import 'package:flutter_chat/screens/login_screen.dart';
+import 'package:flutter_chat/screens/registration_screen.dart';
+import 'package:flutter_chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyBqye15Fvtz7AWGz3RCNc3k8qTcREBiGqs",
+          appId: "1:544849865097:android:50efbc11c49270f3331958",
+          messagingSenderId: "544849865097",
+          projectId: "flash-chat-c38dc"));
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen()
+      },
+    );
+  }
+}
---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_chat/components/rounded_button.dart';
+import 'package:flutter_chat/constans.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'chat_screen.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static const String id = 'registration_screen';
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  bool showSpinner = false;
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  @override
+  void dispose() {
+    email.dispose();
+    password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                controller: email,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your email.'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                controller: password,
+                textInputAction: TextInputAction.done,
+                textAlign: TextAlign.center,
+                obscureText: true,
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your password.'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                  colour: Colors.blueAccent,
+                  title: 'Register',
+                  onPressed: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      final newUser = await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: email.text.trim(),
+                              password: password.text.trim());
+                      if (newUser != null) {
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
